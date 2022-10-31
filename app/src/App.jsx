f9ee58f40c9774ef0ac99704c45785b1d6227e19
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2022-10-31 20:45:22 +0100

    added pagination functionality

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,39 @@
 import "./App.css";
 import axios from "axios";
-import Item from "./components/Item";
 import { useState } from "react";
 import SearchForm from "./components/SearchForm";
+import ReactPaginate from "react-paginate";
+import Card from "./components/UI/Card";
 
 export default function App() {
   const [searchValue, setSearchValue] = useState();
+  const [page, setPage] = useState();
   const saveSearchValueHandler = (enteredSearchValue) => {
     setSearchValue(enteredSearchValue);
   };
   //seems like query is ignoring dashes
-  const url = `https://api.punkapi.com/v2/beers?beer_name=${searchValue}`;
+  const pageLimit = 10;
+  const url = `https://api.punkapi.com/v2/beers?beer_name=${searchValue}&per_page=${pageLimit}`;
   const [data, setData] = useState([]);
-  const fetchData = async (u) => {
-    return await axios.get(u).then((res) => {
+  const fetchData = async (url) => {
+    return await axios.get(url).then((res) => {
       setData(res.data);
     });
+  };
+  const fetchNextPage = async (currentPage) => {
+    await axios
+      .get(
+        `https://api.punkapi.com/v2/beers?beer_name=${searchValue}&per_page=${pageLimit}&page=${currentPage}`
+      )
+      .then((res) => {
+        setData(res.data);
+      });
+  };
+
+  const pageClickHandler = async (data) => {
+    let currentPage = data.selected + 1;
+    const pageServer = await fetchNextPage(currentPage);
+    setPage(pageServer);
   };
 
   //Varje gång submit noll
@@ -25,6 +43,25 @@ export default function App() {
         onSubmitSearchValue={saveSearchValueHandler}
         onSearch={fetchData}
         url={url}
+      />
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        breakLabel={"..."}
+        pageCount={6}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={pageClickHandler}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        activeClassName={"active"}
       />
       {data?.map((beer, index) => {
         return (
@@ -40,3 +77,23 @@ export default function App() {
     </div>
   );
 }
+const Item = (props) => {
+  return (
+    <Card>
+      <ul>
+        <li>Name: {props.name}</li>
+        <li>Alcohol content: {props.abv}%</li>
+        <details>
+          <summary>Description</summary>
+          {props.description}
+        </details>
+        <details>
+          <summary>Food pairing</summary>
+          {props.foodpairing.map((pairing) => {
+            return `${pairing}, `;
+          })}
+        </details>
+      </ul>
+    </Card>
+  );
+};
--- app/src/components/UI/Card.jsx
+++ app/src/components/UI/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 import React from "react";
 
 const Card = (props) => {
-  return <div className="card">{props.children}</div>;
+  return <div className="componentCard">{props.children}</div>;
 };
 
 export default Card;
